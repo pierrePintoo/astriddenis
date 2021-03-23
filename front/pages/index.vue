7505d7f76f3aecbc9a3f10651305353b4e10d668
Pierrot <p.pinto2oliveira@gmail.com>
2021-03-23 20:53:08 +0100

    Init front 🎉

---
+++ front/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <ul v-else>
+      <li v-for="article in articles" :key="article.id">
+        {{ article.apropos }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      articles: [],
+      error: null
+    }
+  },
+  async mounted () {
+    try {
+      this.articles = await this.$strapi.$articles.find()
+      console.log(this.articles)
+    } catch (error) {
+      this.error = error
+    }
+  }
+}
+</script>
