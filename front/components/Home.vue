51f30f3049d0c6bf1eecc7626f928bfd755d06d3
Pierrot <p.pinto2oliveira@gmail.com>
2021-03-27 22:40:32 +0100

    🔧 Config tailwind colors

--- front/components/Home.vue
+++ front/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home bg-pink">
     <!-- <div v-if="error">
       {{ error }}
     </div>
---
+++ front/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  theme: {
+    colors: {
+      pink: '#FFBFBF'
+    }
+  }
+}
