aa0206a5277edcf860a74e657369cf5d22d9617d
Pierrot <p.pinto2oliveira@gmail.com>
2021-03-28 01:36:51 +0100

    ♻️ Refactor Landing Page

--- front/components/Home.vue
+++ front/components/Home.vue
@@ -1,73 +1,22 @@
 <template>
   <div class="home bg-pink w-screen h-screen">
-    <!-- <div v-if="error">
-      {{ error }}
-    </div>
-    <ul v-else>
-      <li v-for="article in articles" :key="article.id">
-        {{ article.apropos }}
-      </li>
-    </ul> -->
-    <div class="h-full landing_page flex justify-between">
-      <div class="landing_page--left h-full flex flex-col justify-between">
-        <a href="#" class="text-linkBig">Astrido</a>
-        <div>
-          <h1>Hello, c'est Astrido</h1>
-          <h1>Je suis une communicante passionnée</h1>
-        </div>
-        <div class="group flex items-center cursor-pointer">
-          <button class="border border-black rounded-full h-20 w-20"><img src="~/assets/icons/arrow-right.svg" alt="" class="w-8 m-auto transform rotate-90 group-hover:translate-y-2 transition duration-200"></button>
-          <a href="#" class="ml-8 text-linkBig">Check my experiences</a>
-        </div>
-      </div>
-      <div class="landing_page--right flex flex-col items-end">
-        <button class="button__top text-small border border-black rounded-full h-16">En</button>
-        <div class="nav border-r border-black h-full">
-          <ul class="h-full flex flex-col justify-between items-end">
-            <li class="nav__item text-small flex items-center"><a href="#" class="font-bold">Home</a><span class="nav__item__right absolute w-6 h-6 border border-black rounded-full bg-black mb-1"></span></li>
-            <li class="nav__item text-small flex items-center"><a href="#">Mes expériences</a><span class="nav__item__right absolute w-6 h-6 border border-black rounded-full bg-pink"></span></li>
-            <li class="nav__item text-small flex items-center"><a href="#">Contact</a><span class="nav__item__right absolute w-6 h-6 border border-black rounded-full bg-pink mt-1"></span></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <LandingBlock />
   </div>
 </template>
 
 <script>
+import LandingBlock from "./LandingBlock/LandingBlock"
+
 export default {
   name: 'Home',
   data () {
     return {
-      articles: [],
-      error: null
     }
   },
-  async mounted () {
-    try {
-      this.articles = await this.$strapi.$articles.find()
-      console.log(this.articles)
-    } catch (error) {
-      this.error = error
-    }
-  }
 }
 </script>
 <style scoped>
   .home {
     padding: 3.75rem 8.75rem 6rem 8.75rem;
-  }
-
-  .nav {
-    margin-top: 5.5rem;
-    padding-right: 3.5rem;
-  }
-
-  .nav__item__right {
-    right : 8.0625rem
-  }
-
-  .button__top {
-    width: 3.375rem;
   }
 </style>
