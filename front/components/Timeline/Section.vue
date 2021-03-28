e40a985acca4b800c3107f476ddda2abf629cb89
Pierrot <p.pinto2oliveira@gmail.com>
2021-03-28 03:32:05 +0200

    ✨ Section component

---
+++ front/components/Timeline/Section.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section">
+      <h1>Coucou</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Section',
+  data () {
+    return {
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
