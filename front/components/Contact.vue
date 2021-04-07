c25203471ccb202f83b2c27661cb50ad5d81b5e0
Pierrot <p.pinto2oliveira@gmail.com>
2021-04-07 09:46:13 +0200

    ✨ Contact component

---
+++ front/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :style="{height: `${this.windowHeight + 60}px`}" class="contact">
+      <h1>Contact</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Contact',
+  props: ["windowHeight"],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
--- front/components/Content.vue
+++ front/components/Content.vue
@@ -2,15 +2,17 @@
     <div class="content">
         <LandingBlock :windowHeight = this.windowHeight />
         <Section :windowHeight = this.windowHeight />
+        <Contact :windowHeight = this.windowHeight />
     </div>
 </template>
 <script>
     import LandingBlock from "./LandingBlock/LandingBlock"
     import Section from "./Timeline/Section"
+    import Contact from "./Contact"
     
     export default {
         name: 'Content',
-        components: { Section },
+        components: { Section, Contact },
         props: ['windowHeight'],
         data () {
             return {
