d82f17670472c7df626edaab74e0033f56aeb2e0
Pierrot <p.pinto2oliveira@gmail.com>
2021-05-01 18:02:03 +0200

    ✨ Dynamic contact content with api

--- front/components/Contact.vue
+++ front/components/Contact.vue
@@ -3,21 +3,46 @@
     <div class="contact__left h-full flex flex-col">
         <a href="#" class="contact__left__top text-linkBig mb-20">Astrido</a>
         <div class="contact__left__center">
-          <h2 class="mb-20">Vous pouvez me joindre, mais soyez gentils.</h2>
-          <h1 class="underline"><a href="mailto:astriddns@gmail.com">astriddns@gmail.com</a></h1>
-          <h1 class="underline"><a href="tel:+33 6 87 50 43 56">+33 6 87 50 43 56</a></h1>
+          <h2 class="mb-20">{{ contactContent.catch_phrase }}</h2>
+          <h1 class="underline"><a href="mailto:astriddns@gmail.com">{{ contactContent.Email }}</a></h1>
+          <h1 class="underline"><a :href="'tel:' + contactContent.telephone">{{ contactContent.telephone }}</a></h1>
         </div>
-        <p class="contact__left__bottom text-linkBig mt-auto mb-24">Paris</p>
+        <p class="contact__left__bottom text-linkBig mt-auto mb-24">{{ contactContent.place }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios"
 
 export default {
   name: 'Contact',
   data () {
     return {
+      contactContent: [] 
+    }
+  },
+  async beforeMount () {
+    let response = this.getDatas('http://localhost:1337/contact')
+    response.then( value => this.contactContent = value )
+    this.$store.watch(() => {
+      if(this.$store.state.language === 'en') {
+          let response = this.getDatas('http://localhost:1337/contact?_locale=en')
+          response.then( value => this.contactContent = value )
+        } else if (this.$store.state.language === 'fr') {
+          let response = this.getDatas('http://localhost:1337/contact')
+          response.then( value => this.contactContent = value )
+        }
+    })
+  },
+  methods: {
+    getDatas: async function(url) {
+      try {
+          const response = await axios.get(url)
+          return response.data
+      } catch (error) {
+          console.log(error)
+      }
     }
   }
 }
