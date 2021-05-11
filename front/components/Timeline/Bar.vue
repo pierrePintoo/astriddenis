9e4318ebe77cc4d83b151bb6775f8ca96f3d9b97
Pierrot <p.pinto2oliveira@gmail.com>
2021-05-11 11:42:31 +0200

    ✨ Update bullets according to scroll

--- front/components/Timeline/Bar.vue
+++ front/components/Timeline/Bar.vue
@@ -4,15 +4,15 @@
           <button @click="switchLanguage()" v-if="$store.state.language === 'fr'" class="btn bar__top relative pr-1">EN</button>
           <button @click="switchLanguage()" v-else class="btn bar__top relative pr-1">FR</button>
         </div>
-        <div class="nav border-r border-black">
+        <div class="nav border-r border-black" >
           <ul class="h-full flex flex-col justify-between items-end">
             <li @click.stop="onLandingBlockClick()" class="nav__item nav__item--first text-small flex items-center">
-                <a class="nav__item__label" :class="{labelActive: isActive.landingBlock}"> {{ homeContent.home_bar_label}}
-                    <span class="nav__item__right landing__block__item__inner absolute w-6 h-6 border border-black rounded-full bg-pink mb-1" :class="{active: isActive.landingBlock}"></span>
+                <a class="nav__item__label"> {{ homeContent.home_bar_label}}
+                    <span class="nav__item__right landing__block__item__inner absolute w-6 h-6 border border-black rounded-full bg-pink mb-1"></span>
                 </a>
             </li>
             <transition name="fade">
-                <li @click.stop="handleExperiences('click')" v-if="notShowExperiences" class="nav__item text-small flex items-center">
+                <li @click.stop="handleExperiences('click', $event)" v-if="notShowExperiences" class="nav__item text-small flex items-center">
                     <a class="nav__item__label"> {{ homeContent.experiences_bar_label}}
                         <span class="nav__item__right absolute w-6 h-6 border border-black rounded-full bg-pink mb-1" ></span>
                     </a>
@@ -20,16 +20,16 @@
             </transition>
             <transition name="fade">
                 <div v-show="showExperiences" class="h-full flex flex-col justify-around" ref="experiences">
-                    <li v-for="experience in experiences" :key="experience.id" @click.stop="handleExperience($event, 'click', experience.id)" :id="'section__item' + experience.id" class="nav__item section__item text-small flex items-center">
+                    <li v-for="experience in experiences" :key="experience.id" @click.stop="handleExperience($event, 'click', experience.id)" class="nav__item section__item text-small flex items-center">
                         <a class="nav__item__label">{{ experience.title }}
-                            <span class="nav__item__right absolute w-6 h-6 border border-black rounded-full bg-pink mb-1"></span>
+                            <span :id="'nav__item__right' + experience.id" class="nav__item__right absolute w-6 h-6 border border-black rounded-full bg-pink mb-1"></span>
                         </a>
                     </li>
                 </div>
             </transition>
             <li @click.stop="onContactClick()" class="nav__item nav__item--last text-small flex items-center">
-                <a class="nav__item__label" :class="{labelActive: isActive.contact}"> {{ homeContent.contact_bar_label}} 
-                    <span class="nav__item__right absolute w-6 h-6 border border-black rounded-full bg-pink mt-1" :class="{active: isActive.contact}"></span>
+                <a class="nav__item__label"> {{ homeContent.contact_bar_label}} 
+                    <span class="nav__item__right absolute w-6 h-6 border border-black rounded-full bg-pink mt-1"></span>
                 </a>
             </li>
           </ul>
@@ -56,7 +56,8 @@
                 notShowExperiences: true,
                 homeContent: [],
                 scroll,
-                sectionCounter: null
+                sectionCounter: null,
+                sections: null
             }
         },
         methods: {
@@ -69,18 +70,15 @@
                 }, 500)
                 this.isActive.landingBlock = true
             },
-            handleExperiences: function(handleType) {
+            handleExperiences: function(handleType, e) {
                 if(handleType === "click") {
                     this.goToSection(this.experiences[0].id)
-                    this.resetActivesClass()
-                    this.notShowExperiences = false
-                    setTimeout(() => this.showExperiences = true, 500)
-                    setTimeout(() => this.$refs.experiences.children[0].children[0].children[0].classList.add('active'), 600)
                 } else if (handleType === "scroll"){
                     this.resetActivesClass()
                     this.notShowExperiences = false
                     setTimeout(() => this.showExperiences = true, 500)
-                    setTimeout(() => this.$refs.experiences.children[0].children[0].children[0].classList.add('active'), 600)
+                    this.$refs.experiences.children[0].children[0].children[0].classList.add('active')
+                    this.$refs.experiences.children[0].classList.add('labelActive')
                 }
             },
             onContactClick: function() {
@@ -102,20 +100,21 @@
                 } else if (handleType === "scroll") {
                     if(e.localName === "span") {
                         e.classList.add('active')
-                        e.parentElement.classList.add('labelActive')
-                    } else if (e.localName === "a") {
-                        e.children[0].classList.add('active')
-                        e.classList.add('labelActive')
+                        e.parentElement.parentElement.classList.add('labelActive')
                     }
                 }
 
             },
             resetActivesClass: function() {
-                Object.keys(this.isActive).forEach( item => this.isActive[item] = false)
+                // Object.keys(this.isActive).forEach( item => this.isActive[item] = false)
+                
+                // Common item list (first and last)
+                let navItems = document.getElementsByClassName('nav__item')
+                console.log(navItems)
                 if(this.$refs.experiences) {
                                             //li                      //a             //span
-                    this.$refs.experiences.children.forEach( item => item.children[0].children[0].classList.remove('active'))
-                    this.$refs.experiences.children.forEach( item => item.children[0].classList.remove('labelActive'))
+                    navItems.forEach( item => item.children[0].children[0].classList.remove('active') )
+                    navItems.forEach( item => item.classList.remove('labelActive') )
                 }
             },
             switchLanguage: function() {
@@ -157,18 +156,38 @@
         },
         mounted(){
             const windowH = window.innerHeight
+            const windowH2 = windowH / 2
+            let newScrollY = 0
+            let oldScrollY = 0
             window.addEventListener('scroll', e => {
-                if(window.scrollY >= windowH / 2) {
-
-                    this.sectionCounter = 0
+                newScrollY = window.scrollY
+                if(newScrollY > oldScrollY) {
+                    // Scroll down
+                    if(window.scrollY >= windowH / 2 && window.scrollY <= windowH + windowH2) {
+                        this.sectionCounter = 1
+                    } 
+                    if(window.scrollY > windowH * this.sectionCounter + windowH2){
+                        this.sectionCounter++
+                    }
+                } else {
+                    // Scroll up
+                    if(window.scrollY >= windowH / 2 && window.scrollY <= windowH + windowH2) {
+                        this.sectionCounter = 1
+                    } else if (window.scrollY < windowH * (this.sectionCounter - 1) + windowH2) {
+                        this.sectionCounter--
+                    }
                 }
+                oldScrollY = newScrollY
             })
         },
         watch: {
             sectionCounter: function (val) {
-                let sections = document.getElementsByClassName('section__item')
-                this.handleExperiences('scroll')
-                this.handleExperience(sections[val], 'scroll')
+                this.sections = document.getElementsByClassName('nav__item__right')
+                if(val === 1){
+                    this.handleExperiences('scroll')
+                } else if(val < this.sections.length) {
+                    this.handleExperience(this.sections[val], 'scroll')
+                }
             }
         }
     }
